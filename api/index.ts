8a912ff9898253946feba55d81e3656889ef5868
Rishit8934 <rishit8934@gmail.com>
2026-07-03 23:26:04 +0530

    Restore full Express app with all routes

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,17 @@
-import express from "express";
+import express, { type Request, Response, NextFunction } from "express";
+import { createServer } from "http";
+import { registerRoutes } from "../server/routes";
 
 const app = express();
+const httpServer = createServer(app);
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/api/newspapers", (_req, res) => {
-  res.json([{ id: "1", name: "Express works on Vercel!" }]);
+registerRoutes(httpServer, app);
+
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  res.status(err.status || 500).json({ message: err.message || "Internal Server Error" });
 });
-
-app.get("/api/ping", (_req, res) => res.json({ ok: true }));
 
 export default app;
